eb0b802aaa516e643b2d4dc50fa7d2932aa978aa
Muhammad Asadullah Al Galib <galib.bcse@gmail.com>
2018-12-17 04:53:31 +0600

    Initial commit of my webpack 4 with babel 8

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    output: {
+        path: __dirname + '/dist/js',
+        filename: 'main.js'
+    },
+
+    mode: 'development',
+
+    watch: true,
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+}
